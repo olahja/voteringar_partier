82fe4138d0b21f817a8e041cee47bed494504e6d
Ola Hjalmarsson <ola@altinget.se>
2015-07-28 23:43:49 +0200

    started runner

--- votepart_runner.py
+++ votepart_runner.py
@@ -1,6 +1,14 @@
+#from sys import argv
+import argparse
+import url_constructor
+from VotePartListObj import VotePartList
 
 
 
+def main():
+    #parser = argparse.ArgumentParser(description='Program')
+    url = url_constructor.construct_url(partier_list, riksmote_list)
+    utskott = 
 
 
 
@@ -10,5 +18,7 @@
 finish runner
 DONE insert choice to check only vissa utskott
 sys.argv flags etc
+integrate the whole thing with R
 
+                                     
 '''
