1ed2be452aa4fcf8ab7c23913bbe0e6ee66bb7e1
Ola Hjalmarsson <ola@altinget.se>
2015-07-29 03:17:18 +0200

    Added matrix option to cli parsing

--- votepart_runner.py
+++ votepart_runner.py
@@ -9,20 +9,26 @@ import constants
 def main():
     parties = constants.part_abb_list
     parser = argparse.ArgumentParser()
+    riksmote_def = [constants.today_riksmote()]
     parser.add_argument("-p", "--partier", dest="partier", nargs="*", help="add parties", metavar="partier", default=parties)
-    parser.add_argument("-r", "--riksmote", dest="riksmote", nargs="*", help="add riksmoten", metavar="riksmote", default=constants.today_riksmote())
+    parser.add_argument("-r", "--riksmote", dest="riksmote", nargs="*", help="add riksmoten", metavar="riksmote", default=riksmote_def)
     parser.add_argument("-u", "--utskott", dest="utskott", nargs="*", help="add utskott", metavar="utskott", default=None)
+    parser.add_argument("-m", "--matrix", dest="matrix", action='store_true', help="create matrix", default=False)
     
     parsed = parser.parse_args()
     print(parsed.partier)
     print(parsed.riksmote)
     print(parsed.utskott)
+    print(parsed.matrix)
+    
     partier = parsed.partier
     riksmote = parsed.riksmote
     utskott = parsed.utskott
     url = url_constructor.construct_url(partier, riksmote)
-
-main()
+    
+    
+if __name__ == '__main__':
+    main()
 
 ### TODO ###
 '''
